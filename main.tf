a0f1f46618252e2f711b8977223b16303dee3b76
Erik Osterman <erik@cloudposse.com>
2018-10-23 13:06:39 -0700

    Initial implementation (#1)
    * Implement GitHub repository webhooks
    
    * Update readme
    
    * Bump ci
    
    * Fix formatting
    
    * Address PR

---
+++ main.tf
@@ -0,0 +1,26 @@
+provider "github" {
+  token        = "${var.github_token}"
+  organization = "${var.github_organization}"
+}
+
+resource "github_repository_webhook" "default" {
+  count = "${var.enabled == "true" && length(var.github_repositories) > 0 ? length(var.github_repositories) : 0}"
+
+  name       = "${var.name}"
+  repository = "${var.github_repositories[count.index]}"
+  active     = "${var.active}"
+
+  configuration {
+    url          = "${var.webhook_url}"
+    content_type = "${var.webhook_content_type}"
+    secret       = "${var.webhook_secret}"
+    insecure_ssl = "${var.webhook_insecure_ssl}"
+  }
+
+  events = "${var.events}"
+
+  lifecycle {
+    # This is required for idempotency
+    ignore_changes = ["configuration.secret"]
+  }
+}
