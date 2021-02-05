1ef48143b405c10f252aaf3d321b986bc33cd76a
Maxim Mironenko <maxim@cloudposse.com>
2021-02-05 12:54:25 +0700

    context.tf updated to v0.24.1, minimum required Terraform version bumped to 0.13.0 when needed, readme updated (#26)

--- versions.tf
+++ versions.tf
@@ -1,10 +1,11 @@
 terraform {
-  required_version = ">= 0.12.26"
+  required_version = ">= 0.13.0"
 
   required_providers {
     github = {
       source = "hashicorp/github"
       # breaking changes both immediately before and after 3.0.0, pin exactly
+      # until ready to upgrade to integrations/github 4.2 or later
       version = "3.0.0"
     }
     local = {
