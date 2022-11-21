f0d01710dc4232b32f36c436d83dc287dbc23345
slamet kristanto <botoksgonzales@gmail.com>
2022-11-21 14:36:13 +0900

    Fix examples/complete for terratest

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -5,8 +5,6 @@ provider "aws" {
 module "webhooks" {
   source              = "../../"
   active              = false
-  github_token        = var.github_token
-  github_organization = var.github_organization
   github_repositories = var.github_repositories
   webhook_url         = var.webhook_url
   events              = var.events
