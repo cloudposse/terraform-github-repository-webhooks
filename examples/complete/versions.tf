45852dd74546b04785af2136b3aa084d0fc90010
nitrocode <7775707+nitrocode@users.noreply.github.com>
2022-11-20 21:54:58 -0600

    Update versions.tf

--- examples/complete/versions.tf
+++ examples/complete/versions.tf
@@ -1,1 +1,16 @@
-../../versions.tf
+terraform {
+  required_version = ">= 0.13.0"
+
+  required_providers {
+    github = {
+      source = "integrations/github"
+      # breaking changes both immediately before and after 3.0.0, pin exactly
+      # until ready to upgrade to integrations/github 4.2 or later
+      version = ">= 4.2.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = ">= 1.2"
+    }
+  }
+}
